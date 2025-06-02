a79c90e7ea989b6eb3c249505ea897b3c92fc32d
Alaaddin Can GURSOY <EchoTD>
2025-06-02 11:53:04 +0000

    further testing

--- backend/waterQuality/src/main/java/project/waterQuality/config/SecurityConfig.java
+++ backend/waterQuality/src/main/java/project/waterQuality/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
 		http
 				.csrf(csrf -> csrf.disable())
 				.authorizeHttpRequests(auth -> auth
-						.requestMatchers("/api/auth/**").permitAll()
+						.requestMatchers("/api/auth/**", "/actuator/**").permitAll()
 						.anyRequest().authenticated())
 				.sessionManagement(session -> session
 						.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
