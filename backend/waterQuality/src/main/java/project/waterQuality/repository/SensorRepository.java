24948f2f0bf4d350e492eaa158a73d513afe8175
Alaaddin Can Gürsoy <alaaddin.c.gursoy@gmail.com>
2025-06-10 13:49:05 +0300

    feat: reorganized the subfolders with submodules

--- backend/waterQuality/src/main/java/project/waterQuality/repository/SensorRepository.java
+++
@@ -1,9 +0,0 @@
-package project.waterQuality.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import project.waterQuality.model.SensorData;
-
-public interface SensorRepository extends JpaRepository<SensorData, Long> {
-
-}
